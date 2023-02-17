721579f42d8a16d9d54f316186710e598f1edd9d
VitaliyStels <38788206+VitaliyStels@users.noreply.github.com>
2023-02-17 23:29:01 +0200

    Files

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import { FiberContainer } from './FiberContainer'
+function App() {
+	return (
+		<div className="App">
+			<FiberContainer />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/Trees.jsx
@@ -0,0 +1,21 @@
+import React, { useRef } from "react";
+import { useGLTF } from "@react-three/drei";
+import { Color } from "three";
+
+export function Trees(props) {
+	const { nodes, materials } = useGLTF("/trees.glb");
+	return (
+		<group {...props} dispose={null}>
+			<mesh
+				castShadow
+				receiveShadow
+				geometry={nodes.Foliage.geometry}
+				position={[0.33, -0.05, -0.68]}
+			>
+				<meshStandardMaterial color={new Color('#33594e').convertLinearToSRGB()} />
+			</mesh>
+		</group>
+	);
+}
+
+useGLTF.preload("/trees.glb");
